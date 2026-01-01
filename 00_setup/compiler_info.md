3bc30d5cee760218628dd81a25b44ae3c51d72d4
jayanthjinka <jayanthcode0709@gmail.com>
2026-01-01 15:52:02 +0530

    Add compiler setup and first program

---
+++ 00_setup/compiler_info.md
@@ -0,0 +1,11 @@
+# Compiler Information
+
+Operating System : Windows 11
+
+Compiler : Microsoft (R) C/C++ Optimizing Compiler (MSVC)
+
+Compiler Version : 19.50.35721 for x64
+
+Compile Command : cl /EHsc first_program.cpp
+
+Execute Command : first_program.exe
---
+++ 00_setup/first_program.cpp
@@ -0,0 +1,17 @@
+/*
+Topic : First C++ Program
+Purpose : 
+- Verify MSVC compiler setup
+- Understand basic output 
+- Learn C++ program structure
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, this is my first C++ program." << endl;
+    cout << "I am setting up my cpp-basics repository." << endl;
+    cout << "Compiler setup is successful." << endl;
+    return 0;
+}
